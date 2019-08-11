c1c994513cdc80d86329ef2c448c2a9c01879695
heesu-jung <heesu.jung@navercorp.com>
2019-08-11 16:45:53 +0900

    :heavy_plus_sign: typescript

--- src/App.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-const App = props => <div>{props.children}</div>;
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const App: React.FunctionComponent<void> = props => <div>{props.children}</div>;
+export default App;
